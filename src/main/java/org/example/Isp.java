e509534fd6ed90083b69927bbda1085a1aa9b04a
abhigyan-gogoi <abhigyank.gogoi@zirohcom>
2022-10-10 14:29:08 +0530

    Preserving ISP

--- src/main/java/org/example/Isp.java
+++ src/main/java/org/example/Isp.java
@@ -1,20 +1,76 @@
 package org.example;
 
-import javax.crypto.Mac;
-
 // ISP - Interface Segregation Principle
 // Recommendation on how to split interfaces into smaller interfaces
 public class Isp {
 }
 
 class Document {
-
 }
 
 interface Machine {
     void print(Document d);
     void fax(Document d);
     void scan(Document d);
+}
+
+// Preserving ISP
+// Split Machine interface into smaller interfaces
+interface Printer {
+    void print(Document d);
+}
+
+interface Scanner {
+    void scan(Document d);
+}
+
+// Make the class implement just the interface needed
+class JustAPrinter implements Printer {
+
+    @Override
+    public void print(Document d) {
+        // Logic
+    }
+}
+
+// NOTE: YAGNI - You Ain't Going to Need It
+// Preserving ISP by implementing multiple interfaces
+class PhotoCopier implements Printer, Scanner {
+
+    @Override
+    public void print(Document d) {
+        // Logic
+    }
+
+    @Override
+    public void scan(Document d) {
+        // Logic
+    }
+}
+
+// Preserving ISP by implementing an interface which extends multiple interfaces
+interface PhotoCopierInterface extends Printer, Scanner{}
+
+// Using Decorator design pattern
+// Build a decorator: reuse functionality of printer and scanner
+class PhotoCopier2 implements PhotoCopierInterface {
+    private Printer printer;
+    private Scanner scanner;
+
+    public PhotoCopier2(Printer printer, Scanner scanner) {
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void print(Document d) {
+        printer.print(d);
+    }
+
+    @Override
+    public void scan(Document d) {
+        scanner.scan(d);
+    }
 }
 
 class MultiFunctionPrinter implements Machine {
